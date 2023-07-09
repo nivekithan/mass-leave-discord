900dfc73aaf46df003c20e11d48830e35ba0a7f4
Nivekithan S <nivekithan363@gmail.com>
2023-07-09 22:31:12 +0530

    Add select, un select buttons

--- frontend-mass-leave-discord/app/components/guildList.tsx
+++ frontend-mass-leave-discord/app/components/guildList.tsx
@@ -1,36 +1,133 @@
+import { useMemo, useReducer } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Checkbox } from "./ui/checkbox";
 import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+
+function guildListReducer(
+  state: Record<string, boolean>,
+  dispatch:
+    | { type: "set_state"; id: string; value: boolean }
+    | { type: "select_all" }
+    | { type: "unselect_all" }
+) {
+  if (dispatch.type === "set_state") {
+    return { ...state, [dispatch.id]: dispatch.value };
+  }
+
+  if (dispatch.type === "select_all") {
+    const newState: Record<string, boolean> = {};
+
+    Object.keys(state).forEach((id) => {
+      newState[id] = true;
+    });
+    return newState;
+  }
+
+  if (dispatch.type === "unselect_all") {
+    const newState: Record<string, boolean> = {};
+
+    Object.keys(state).forEach((id) => {
+      newState[id] = false;
+    });
+    return newState;
+  }
+
+  return { ...state };
+}
 
 export function GuildList({
   guilds,
 }: {
   guilds: Array<{ iconUrl: string; name: string; id: string }>;
 }) {
+  const [guildItemCheckboxState, dispatchGuildItemCheckboxState] = useReducer(
+    guildListReducer,
+    null,
+    () => {
+      const checkBoxState: Record<string, boolean> = {};
+
+      guilds.forEach((v) => {
+        checkBoxState[v.id] = false;
+      });
+
+      return checkBoxState;
+    }
+  );
+
+  const selectedItems = useMemo(() => {
+    return Object.values(guildItemCheckboxState).filter((v) => v).length;
+  }, [guildItemCheckboxState]);
+
   return (
-    <ol className="flex flex-col gap-y-8">
-      {guilds.map(({ iconUrl, id, name }) => {
-        return (
-          <li
-            key={id}
-            className="flex items-center min-w-[460px] justify-between border px-6 py-3 rounded-md"
+    <div className="flex flex-col gap-y-8">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-x-4">
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => {
+              dispatchGuildItemCheckboxState({ type: "select_all" });
+            }}
           >
-            <div className="flex items-center gap-x-4">
-              <Avatar>
-                <AvatarImage src={iconUrl} />
-                <AvatarFallback>{name.at(0)}</AvatarFallback>
-              </Avatar>
-              <Label
-                htmlFor={id}
-                className="max-w-[400px] overflow-hidden text-ellipsis whitespace-nowrap"
-              >
-                {name}
-              </Label>
-            </div>
-            <Checkbox id={id} name="guildId" value={id} />
-          </li>
-        );
-      })}
-    </ol>
+            Select All
+          </Button>
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => {
+              dispatchGuildItemCheckboxState({ type: "unselect_all" });
+            }}
+          >
+            Unselect All
+          </Button>
+        </div>
+        <Button variant="destructive" disabled={selectedItems <= 0}>
+          <p>Leave from {selectedItems} servers</p>
+        </Button>
+      </div>
+      <ol className="flex flex-col gap-y-8">
+        {guilds.map(({ iconUrl, id, name }) => {
+          return (
+            <li
+              key={id}
+              className="flex items-center min-w-[460px] justify-between border px-6 py-3 rounded-md"
+            >
+              <div className="flex items-center gap-x-4">
+                <Avatar>
+                  <AvatarImage src={iconUrl} />
+                  <AvatarFallback>{name.at(0)}</AvatarFallback>
+                </Avatar>
+                <Label
+                  htmlFor={id}
+                  className="max-w-[400px] overflow-hidden text-ellipsis whitespace-nowrap"
+                >
+                  {name}
+                </Label>
+              </div>
+              <Checkbox
+                id={id}
+                name="guildId"
+                value={id}
+                checked={guildItemCheckboxState[id]}
+                onCheckedChange={(checked) => {
+                  return checked
+                    ? dispatchGuildItemCheckboxState({
+                        type: "set_state",
+                        id,
+                        value: true,
+                      })
+                    : dispatchGuildItemCheckboxState({
+                        id,
+                        value: false,
+                        type: "set_state",
+                      });
+                }}
+              />
+            </li>
+          );
+        })}
+      </ol>
+    </div>
   );
 }
